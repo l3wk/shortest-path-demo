54e3b264e94e483bf046b1f00bbe613cda8180bd
l3wk <l3wk@users.noreply.github.com>
2017-08-27 09:59:10 +1000

    Added new search method to search service interface.

--- src/main/java/io/github/l3wk/spd/search/SearchService.java
+++ src/main/java/io/github/l3wk/spd/search/SearchService.java
@@ -1,5 +1,10 @@
 package io.github.l3wk.spd.search;
 
+import java.util.Stack;
+
+import io.github.l3wk.spd.graph.Vertex;
+
 public interface SearchService {
 
+	public Stack<Vertex> findShortestPath(Vertex source, Vertex target);
 }

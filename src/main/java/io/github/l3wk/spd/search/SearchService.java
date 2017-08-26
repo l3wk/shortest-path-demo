2e30817d8bd0b4e3f6495ab4e75aee1dad6bb3be
l3wk <l3wk@users.noreply.github.com>
2017-08-26 15:26:55 +1000

    Search package setup.

---
+++ src/main/java/io/github/l3wk/spd/search/SearchService.java
@@ -0,0 +1,5 @@
+package io.github.l3wk.spd.search;
+
+public interface SearchService {
+
+}
---
+++ src/test/java/io/github/l3wk/spd/search/DijkstraSearchServiceTestCase.java
@@ -0,0 +1,19 @@
+package io.github.l3wk.spd.search;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DijkstraSearchServiceTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

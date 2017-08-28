6141ba67e587565eb3e9cba7544d3a1b3bad9e67
l3wk <l3wk@users.noreply.github.com>
2017-08-28 22:29:29 +1000

    Test loader behaviour when an empty stream is encountered.

--- src/test/java/io/github/l3wk/spd/loader/JsonGraphLoaderTestCase.java
+++ src/test/java/io/github/l3wk/spd/loader/JsonGraphLoaderTestCase.java
@@ -2,6 +2,8 @@ package io.github.l3wk.spd.loader;
 
 import static org.junit.Assert.*;
 
+import java.util.stream.Stream;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,5 +21,11 @@ public class JsonGraphLoaderTestCase {
 	public void testLoad_nullStream() {
 		
 		assertNull(loader.load(null));
+	}
+	
+	@Test
+	public void testLoad_emptyStream() {
+		
+		assertNull(loader.load(Stream.empty()));
 	}
 }

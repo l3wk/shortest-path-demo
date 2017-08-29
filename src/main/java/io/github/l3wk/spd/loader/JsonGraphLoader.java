76cc33ea99829f10ff3b3d43816cbed665f4ab77
l3wk <l3wk@users.noreply.github.com>
2017-08-29 22:43:35 +1000

    Implement graph loader and tests.

--- src/main/java/io/github/l3wk/spd/loader/JsonGraphLoader.java
+++ src/main/java/io/github/l3wk/spd/loader/JsonGraphLoader.java
@@ -1,14 +1,104 @@
 package io.github.l3wk.spd.loader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.json.JSONObject;
+
+import io.github.l3wk.spd.graph.Edge;
 import io.github.l3wk.spd.graph.Graph;
+import io.github.l3wk.spd.graph.Vertex;
 
 public class JsonGraphLoader implements GraphLoader {
 
+	private static final String USER_KEY = "user";
+	private static final String FRIENDS_KEY = "friends";
+	private static final String SKILL_KEY = "skill";
+	
+	public static final int DEFAULT_SKILL = 0;
+	
+	private AtomicInteger edgeIds = new AtomicInteger();
+	
+	private List<Vertex> vertexes;
+	private List<Edge> edges;
+	private Map<Integer, Integer> skillByUser;
+	
 	@Override
 	public Graph load(Stream<String> stream) {
+		
+		initialise();
+		
+		if (stream != null) {
+			stream.filter(entry -> !entry.isEmpty()).forEach(entry -> {
+								
+				JSONObject json = new JSONObject(entry);
+				
+				if (json.has(USER_KEY)) {
+					int user = loadUser(json);
+					
+					skillByUser.put(user, loadSkill(json));
+					
+					List<Edge> friends = loadFriends(json).stream()
+							.map(friendId -> new Edge(edgeIds.getAndIncrement(), user, friendId, DEFAULT_SKILL))
+							.collect(Collectors.toList());
+					
+					vertexes.add(new Vertex(user, friends.stream().map(friend -> friend.getId()).collect(Collectors.toList())));
+					edges.addAll(friends);
+				}
+			});
 			
-		return null;
+			populateEdgeWeights();
+		}
+		
+		return new Graph(vertexes, edges);
+	}
+	
+	private void initialise() {
+		
+		vertexes = new ArrayList<>();
+		edges = new ArrayList<>();
+		skillByUser = new HashMap<>();
+	}
+	
+	private int loadUser(JSONObject object) {
+		
+		return object.getInt(USER_KEY);
+	}
+	
+	private int loadSkill(JSONObject object) {
+		
+		return object.has(SKILL_KEY) ? object.getInt(SKILL_KEY) : DEFAULT_SKILL;
+	}
+	
+	private List<Integer> loadFriends(JSONObject object) {
+		
+		if (!object.has(FRIENDS_KEY)) {
+			return Collections.<Integer>emptyList();
+			
+		} else {		
+			List<Integer> friends = new ArrayList<>();
+			
+			object.getJSONArray(FRIENDS_KEY).forEach(id -> friends.add((Integer) id));
+			
+			return friends;
+		}
+	}
+	
+	private void populateEdgeWeights() {
+		
+		// populate the graph edge weights with the inverse skill of the edge target
+		
+		OptionalInt maxSkill = skillByUser.values().stream().mapToInt(Integer::intValue).max();
+		
+		if (maxSkill.isPresent()) {
+			edges.forEach(edge -> edge.setWeight(maxSkill.getAsInt() - skillByUser.get(edge.getTargetId())));
+		}
 	}
 }
--- src/test/java/io/github/l3wk/spd/loader/JsonGraphLoaderTestCase.java
+++ src/test/java/io/github/l3wk/spd/loader/JsonGraphLoaderTestCase.java
@@ -2,10 +2,16 @@ package io.github.l3wk.spd.loader;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import io.github.l3wk.spd.graph.Edge;
+import io.github.l3wk.spd.graph.Graph;
+import io.github.l3wk.spd.graph.Vertex;
 
 public class JsonGraphLoaderTestCase {
 
@@ -20,12 +26,202 @@ public class JsonGraphLoaderTestCase {
 	@Test
 	public void testLoad_nullStream() {
 		
-		assertNull(loader.load(null));
+		Graph graph = loader.load(null);
+		
+		assertNotNull(graph);
+		assertTrue(graph.isEmpty());
 	}
 	
 	@Test
 	public void testLoad_emptyStream() {
 		
-		assertNull(loader.load(Stream.empty()));
+		Graph graph = loader.load(Stream.empty());
+		
+		assertNotNull(graph);
+		assertTrue(graph.isEmpty());
+	}
+	
+	@Test
+	public void testLoad_emptyString() {
+		
+		Graph graph = loader.load(Stream.of(""));
+		
+		assertNotNull(graph);
+		assertTrue(graph.isEmpty());
+	}
+	
+	@Test
+	public void testLoad_emptyEntry() {
+		
+		Graph graph = loader.load(Stream.of("{}"));
+		
+		assertNotNull(graph);
+		assertTrue(graph.isEmpty());
+	}
+	
+	@Test
+	public void testLoad_singleVertexNoEdges() {
+		
+		Graph graph = loader.load(Stream.of("{\"user\": 1}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(1, vertexes.size());
+		assertVertex(vertexes.get(0), 1);
+		
+		assertTrue(graph.getEdges().isEmpty());
+	}
+	
+	@Test
+	public void testLoad_singleVertexEmptyFriends() {
+		
+		Graph graph = loader.load(Stream.of("{\"user\": 1, \"friends\": []}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(1, vertexes.size());
+		assertVertex(vertexes.get(0), 1);
+		
+		assertTrue(graph.getEdges().isEmpty());
+	}
+	
+	@Test
+	public void testLoad_multipleVertexesNoEdges() {
+		
+		Graph graph = loader.load(Stream.of("{\"user\": 1}", "{\"user\": 2}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(2, vertexes.size());
+		assertVertex(vertexes.get(0), 1);
+		assertVertex(vertexes.get(1), 2);
+		
+		assertTrue(graph.getEdges().isEmpty());
+	}
+	
+	@Test
+	public void testLoad_multipleVertexesSingleEdgeWithoutWeight() {
+		
+		Graph graph = loader.load(Stream
+				.of("{\"user\": 1, \"friends\": [2]}", 
+					"{\"user\": 2}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(2, vertexes.size());
+		assertVertex(vertexes.get(0), 1, 1);
+		assertVertex(vertexes.get(1), 2);
+		
+		List<Edge> edges = new ArrayList<>(graph.getEdges());
+		
+		assertEquals(1, edges.size());
+		assertEdge(edges.get(0), 1, 2, JsonGraphLoader.DEFAULT_SKILL);
+	}
+	
+	@Test
+	public void testLoad_multipleVertexesSingleEdgeWithWeights() {
+		
+		Graph graph = loader.load(Stream
+				.of("{\"user\": 1, \"friends\": [2], \"skill\": 10}", 
+					"{\"user\": 2, \"skill\": 2}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(2, vertexes.size());
+		assertVertex(vertexes.get(0), 1, 1);
+		assertVertex(vertexes.get(1), 2);
+		
+		List<Edge> edges = new ArrayList<>(graph.getEdges());
+		
+		assertEquals(1, edges.size());
+		assertEdge(edges.get(0), 1, 2, 8);
+	}
+	
+	@Test
+	public void testLoad_multipleVertexesMultipleEdgesWithoutWeight() {
+		
+		Graph graph = loader.load(Stream
+				.of("{\"user\": 1, \"friends\": [2, 3]}", 
+					"{\"user\": 2, \"friends\": [3]}", 
+					"{\"user\": 3}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(3, vertexes.size());
+		assertVertex(vertexes.get(0), 1, 2);
+		assertVertex(vertexes.get(1), 2, 1);
+		assertVertex(vertexes.get(2), 3);
+		
+		List<Edge> edges = new ArrayList<>(graph.getEdges());
+		
+		assertEquals(3, edges.size());
+		assertEdge(edges.get(0), 1, 2, JsonGraphLoader.DEFAULT_SKILL);
+		assertEdge(edges.get(1), 1, 3, JsonGraphLoader.DEFAULT_SKILL);
+		assertEdge(edges.get(2), 2, 3, JsonGraphLoader.DEFAULT_SKILL);
+	}
+	
+	@Test
+	public void testLoad_multipleVertexesMultipleEdgesWithWeights() {
+		
+		Graph graph = loader.load(Stream
+				.of("{\"user\": 1, \"friends\": [2, 3], \"skill\": 10}", 
+					"{\"user\": 2, \"friends\": [3], \"skill\": 7}", 
+					"{\"user\": 3, \"skill\": 4}"));
+		
+		assertNotNull(graph);
+		assertFalse(graph.isEmpty());
+		
+		List<Vertex> vertexes = new ArrayList<>(graph.getVertexes());
+		
+		assertEquals(3, vertexes.size());
+		assertVertex(vertexes.get(0), 1, 2);
+		assertVertex(vertexes.get(1), 2, 1);
+		assertVertex(vertexes.get(2), 3);
+		
+		List<Edge> edges = new ArrayList<>(graph.getEdges());
+		
+		assertEquals(3, edges.size());
+		assertEdge(edges.get(0), 1, 2, 3);
+		assertEdge(edges.get(1), 1, 3, 6);
+		assertEdge(edges.get(2), 2, 3, 6);
+	}
+	
+	private void assertVertex(Vertex vertex, Integer expectedId) {
+		
+		assertVertex(vertex, expectedId, 0);
+	}
+	
+	private void assertVertex(Vertex vertex, Integer expectedId, int expectedEdgeIdCount) {
+		
+		assertNotNull(vertex);
+		assertEquals(expectedId, vertex.getId());
+		assertEquals(expectedEdgeIdCount, vertex.getEdgeIds().size());
+	}
+	
+	private void assertEdge(Edge edge, Integer expectedSourceId, Integer expectedTargetId, int expectedWeight) {
+		
+		assertNotNull(edge);
+		assertNotNull(edge.getId());
+		assertEquals(expectedSourceId, edge.getSourceId());
+		assertEquals(expectedTargetId, edge.getTargetId());
+		assertEquals(expectedWeight, edge.getWeight());
 	}
 }
